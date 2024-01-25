753d78eaf5c324d97aa87c4b574f24a65df29496
AnuragYdv <anuragyd.96@gmail.com>
2024-01-25 22:49:12 +0530

    Creating a new Java Project  for Practicing

---
+++ src/main/java/com/scaledatum/App.java
@@ -0,0 +1,13 @@
+package com.scaledatum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
