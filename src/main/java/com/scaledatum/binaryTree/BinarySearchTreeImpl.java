74aea5c3fcaed83ab6c3eb668271f8105a0a964e
AnuragYdv <anuragyd.96@gmail.com>
2024-01-28 15:07:51 +0530

    adding BST implementation

--- src/main/java/com/scaledatum/binaryTree/BinarySearchTreeImpl.java
+++ src/main/java/com/scaledatum/binaryTree/BinarySearchTreeImpl.java
@@ -2,7 +2,14 @@ package com.scaledatum.binaryTree;
 
 public class BinarySearchTreeImpl {
     public static void main(String[] args) {
-        Node rootNode=new Node(7);
+        Node rootNode=new Node(100);
+        rootNode.insert(95);
+        rootNode.insert(90);
+        rootNode.insert(99);
+        rootNode.insert(105);
+        rootNode.insert(101);
+        rootNode.insert(110);
+
         System.out.print(rootNode.toString());
     }
 }
--- src/main/java/com/scaledatum/binaryTree/Node.java
+++ src/main/java/com/scaledatum/binaryTree/Node.java
@@ -1,20 +1,51 @@
 package com.scaledatum.binaryTree;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+import java.util.ArrayList;
+
+
 public class Node {
     Node leftChild;
     Node rightChild;
-    int data;
-
-    Node(int  input) {
+    private int data;
+    private static StringBuilder str=new StringBuilder();
+    //StringBuilder str=new StringBuilder();
+    Node(int input) {
         this.data = input;
-        this.leftChild = null;
-        this.rightChild = null;
+        leftChild = null;
+        rightChild = null;
     }
 
+    public void insert(int input) {
+        if (input < this.data) {
+            if (!(this.leftChild == null)) {
+                this.leftChild.insert(input);
+            } else {
+                this.leftChild = new Node(input);
+            }
+        } else {
+            if (!(this.rightChild == null)) {
+                this.rightChild.insert(input);
+            } else {
+                this.rightChild = new Node(input);
+            }
+        }
+    }
+
+    @Override
     public String toString() {
+        if (this.leftChild != null) {
+            this.leftChild.toString();
+        }
 
-        return String.valueOf(this.data);
+        str.append(this.data).append("\n");
 
+        //System.out.println(this.data);
+        if (this.rightChild != null) {
+            this.rightChild.toString();
+        }
 
+        return str.toString();
     }
 }
