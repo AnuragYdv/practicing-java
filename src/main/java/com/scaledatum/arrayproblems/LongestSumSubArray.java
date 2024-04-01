a7a1a660fab65adc0e3eabe6bdeeaee0f9d875b0
AnuragYdv <anuragyd.96@gmail.com>
2024-04-01 14:47:43 +0530

    longestSum Sub Array

---
+++ src/main/java/com/scaledatum/arrayproblems/LongestSumSubArray.java
@@ -0,0 +1,33 @@
+package com.scaledatum.arrayproblems;
+/*
+* https://www.naukri.com/code360/problems/longest-subarray-with-sum-k_6682399
+*
+* You are given an array 'a' of size 'n' and an integer 'k'.
+
+Find the length of the longest subarray of 'a' whose sum is equal to 'k'.
+
+Example :
+Input: ‘n’ = 7 ‘k’ = 3
+‘a’ = [1, 2, 3, 1, 1, 1, 1]
+
+Output: 3
+* */
+public class LongestSumSubArray {
+public int longestSubarrayWithSumK(int[] a, long k) {
+   int longestSubarray=0;
+   // {8,15,17,0,11}
+   for(int i=0;i<a.length;i++){
+       int sum=0;
+       for(int j=i;j<a.length;j++){
+           sum=sum+a[j];
+           if(sum==(int)k){
+              int arr_len=j-i+1;
+               longestSubarray=Integer.max(longestSubarray,arr_len);
+               continue;
+           }
+       }
+   }
+    return longestSubarray;
+}
+}
+
--- src/main/java/com/scaledatum/arrayproblems/Solution.java
+++ src/main/java/com/scaledatum/arrayproblems/Solution.java
@@ -28,10 +28,20 @@ public class Solution {
 //        System.out.println(missingNum.missingNumOP2(nums));
 
         /*************Single Appearence *******************/
-        int[] nums={2,2,1};
-        int[] nums2= {4,1,2,1,2};
-        int[] nums3= {1};
-        SingleAppearence sapr=new SingleAppearence();
-        System.out.println(sapr.numAppearOnceOP2(nums2));
+//        int[] nums={2,2,1};
+//        int[] nums2= {4,1,2,1,2};
+//        int[] nums3= {1};
+//        SingleAppearence sapr=new SingleAppearence();
+//        System.out.println(sapr.numAppearOnceOP2(nums2));
+
+
+        /*************Longest SubArray *******************/
+        LongestSumSubArray lngSubArr=new LongestSumSubArray();
+        int[] nums= {1, 2, 3, 1, 1, 1, 1};
+        int k=3;
+        int[] nums2= {8,15,17,0,11};
+        int k2=17;
+
+        System.out.println(lngSubArr.longestSubarrayWithSumK(nums2,k2));
     }
 }
